6b90aa95a2cf4a2fcffb32bcbebf48da59b17a0e
Asikur Rahman <asikurrahman@Asikurs-MacBook-Pro.local>
2020-03-03 23:40:29 -0800

    update

--- a6/sketch-a6.js
+++ a6/sketch-a6.js
@@ -1,5 +1,5 @@
 
-// Asikur Rahman 
+// Asikur Rahman
 // Assignment 6
 // Person I Collaborated with: Sabreen
 
@@ -85,48 +85,62 @@ function graphData(newData) {
   }
 }
 
-var onRect = false;
-var rectX1;
-var rectX2;
-var rectY;
-var rectW;
-var rectH;
+
+var onRect1; // variable to check if mouse is on top of rectangle
+var onRect2; // variable to check if mouse is on top of rectangle
+var rectX1; // x position of recangle
+var rectX2; // x position of rectangle
+var rectY1; // y pos of rectangle
+var rectY2; // y pos of rectangle
+var rectW; // width of rectangle
+var rectH; // height of rectangle
+
+// draw function used to create the rectangles
 function draw() {
   background(255,255,255);
   if (datain2 > 0){
-	 rectX1 = datain1;
-	 rectX2 = datain2;
-	 rectY = 30;
+	 rectX1 = 300;
+	 rectX2 = 300;
+	 rectY1 = 30;
+	 rectY2 = 200;
      rectW = 100;
      rectH = 100;
-	 fill(255, 204, 0);
-	 rect(rectX1, rectY, rectW, rectH);
-	 fill(0, 255, 204);
-	 rect(rectX2, rectY + 150, rectW, rectH);
+	 fill((datain1-990) * 10, 0, 0);     // same thing as map function 0 to 250
+	 rect(rectX1, rectY1, rectW, rectH); // construct rectangle with my constants
+
+	 fill(0, (datain2 - 800) * 5, 0);    // same thing as map function 0 to 250
+	 rect(rectX2, rectY2, rectW, rectH); // construct rectangle with my constants
+
 	//text(("2 sensors added together: " + datain), datain/5,//30);
   }
 }
 
-
+// this is a function for when mouse is clicked
 function mouseClicked(){
-	if (onRect){
-		//sendLightMessage(1);
-		console.log('sendLightMessage(1)');
+	var info;
+	if (onRect1){ // if mouse clicked rect 1
+		info = 0;   // sends 0 to arduino code
+	}
+
+	if (onRect2){
+		info = 1;
+	}
+
+	if (onRect1 || onRect2){ // if mouse is on neither of them -- don't send anything
+		serial.write(info); // serial writes to do nothing
 	}
 }
 
-function mouseReleased(){
-	if (onRect){
-		console.log('sendLightMessage(0)');
-		//sendLightMessage(0);
-	}
-}
-
+// everytime you move your mouse, it updates if your mouse is on rect 1 or rect 2
 function mouseMoved(){
-	console.log('mouse', mouseX, mouseY);
-	onRect = rectX1 < mouseX &&
-		mouseX < (rectX1 + rectW) &&
-		rectY < mouseY &&
-		mouseY < (rectY + rectH);
-	console.log('onRect', onRect);
+	onRect1 = rectX1 < mouseX && // checks if x pos is true
+		mouseX < (rectX1 + rectW) &&  // checks if width of rect is true
+		rectY1 < mouseY &&           // checks if y pos of rect is true
+		mouseY < (rectY1 + rectH);   // checks if height of rect is true
+
+	onRect2 = rectX2 < mouseX &&    // checks if x pos is true
+		mouseX < (rectX2 + rectW) &&  // checks if width of rect is true
+		rectY2 < mouseY &&            // checks if y pos of rect is true
+		mouseY < (rectY2 + rectH);    // checks if height of rect is true
+
 }
